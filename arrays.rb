4e225302dfda1374a097f9ee433747012f74807f
Kam Kennedy <kameronk013@gmail.com>
2023-07-11 21:46:11 -0600

    add four arrays

---
+++ arrays.rb
@@ -0,0 +1,6 @@
+#practice with arrays and disc golf!!
+
+disc_molds = ["wizzard", "harp", "river", "destroyer", "rive"]
+disc_speeds = [1, 4, 7, 11, 13]
+disc_ratings = [4.8, 3.5, 4.5, 2.2]
+is_it_blue = [false, false, true, true, true]
